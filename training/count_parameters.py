ac4df5528ba3afa61db88e85091d1c1148aee7c9
Gyanam09 <gyanamjain@gmail.com>
2025-09-21 01:26:59 +0530

    no of parameters to be trained without pretrained parameters

--- training/count_parameters.py
+++ training/count_parameters.py
@@ -12,23 +12,23 @@ def count_parameters(model):
 
     total_params = 0
     for name, param in model.named_parameters():
-        if param.requires_grad:
-            param_count = param.numel()
-            total_params += param.numel()
-        
-            if 'text_encoder' in name:
-                params_dict['text_encoder'] += param_count
-            elif 'video_encoder' in name:
-                params_dict['video_encoder'] += param_count
-            elif 'audio_encoder' in name:
-                params_dict['audio_encoder'] += param_count
-            elif 'fusion_layer' in name:
-                params_dict['fusion_layer'] += param_count
-            elif 'emotion_classifier' in name:
-                params_dict['emotion_classifier'] += param_count
-            elif 'sentiment_classifier' in name:
-                params_dict['sentiment_classifier'] += param_count
-        
+        # if param.requires_grad:
+        param_count = param.numel()
+        total_params += param.numel()
+    
+        if 'text_encoder' in name:
+            params_dict['text_encoder'] += param_count
+        elif 'video_encoder' in name:
+            params_dict['video_encoder'] += param_count
+        elif 'audio_encoder' in name:
+            params_dict['audio_encoder'] += param_count
+        elif 'fusion_layer' in name:
+            params_dict['fusion_layer'] += param_count
+        elif 'emotion_classifier' in name:
+            params_dict['emotion_classifier'] += param_count
+        elif 'sentiment_classifier' in name:
+            params_dict['sentiment_classifier'] += param_count
+    
     return params_dict, total_params
 
 if __name__ == "__main__":
